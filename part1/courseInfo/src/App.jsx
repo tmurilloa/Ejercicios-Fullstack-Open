675c58d0eee2d052b932adb83026846344a33e04
AMD <tmurilloa@unal.edu.co>
2025-03-21 17:07:33 -0500

    Ejercicios hechos de la parte 1B

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -6,27 +6,30 @@ const Header = ({course}) => {
   )
 }
 
-const Part = ({part, exercises}) => {
+const Part = ({name, exercises}) => {
   return(
     <p>
-      {part} {exercises}
+      {name} {exercises}
     </p>
   )
 
 }
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
-
+const Content = (parts) => {
+  const part1 = parts.parts[0]
+  const part2 = parts.parts[1]
+  const part3 = parts.parts[2]
   return(
     <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
+      <Part name={part1.name} exercises={part1.exercises}/>
+      <Part name={part2.name} exercises={part2.exercises}/>
+      <Part name={part3.name} exercises={part3.exercises}/>
     </div>
   )
 }
 
-const Total = ({array_exercises}) => {
-  const suma = array_exercises.reduce((acc, num) => acc + num,0 )
+const Total = (props) => {
+  const array = props.parts
+  const suma = array[0].exercises + array[1].exercises + array[2].exercises 
   return(
     <p>
       Numer of exercises {suma}
@@ -36,19 +39,27 @@ const Total = ({array_exercises}) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course ={
+    name: 'Half Stack application development',
+    parts: [ {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total array_exercises={[exercises1, exercises2, exercises3]}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
