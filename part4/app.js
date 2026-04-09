2d1f6632de30ce170baeb5eaabbcaad738471bb1
AMD <tmurilloa@unal.edu.co>
2026-04-08 21:08:04 -0500

    Hecho los ejercicios hasta 4.2 Lista de Blogs, paso 2

---
+++ part4/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDb', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+
+module.exports = app
