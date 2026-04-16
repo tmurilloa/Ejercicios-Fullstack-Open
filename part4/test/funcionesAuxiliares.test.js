52c92b96fa4bf48466498ab8e6ac290e285af08f
tmurilloa <tmurilloa@unal.edu.co>
2026-04-15 22:05:34 -0500

    Hecho los ejercicio hasta 4.7 Funciones Auxiliares y Pruebas Unitarias, paso 5

--- part4/test/funcionesAuxiliares.test.js
+++ part4/test/funcionesAuxiliares.test.js
@@ -2,7 +2,6 @@ const { test, describe} = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 const logger = require('../utils/logger')
-const { title } = require('node:process')
 
 test('dummy returns one', () => {
     const blogs = []
@@ -135,5 +134,143 @@ describe('Favorite blog', () => {
     }
 
     assert.deepStrictEqual(result,favoriteBlog_Assert)
+  })
+})
+
+describe('Most blogs', () => {
+    const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+  test('Con la funcion hecha por logica mostBlogs', () => {
+    const result = listHelper.mostBlogs(blogs)
+    logger.info(result)
+    const mostBlogs_Assert = {
+      author: "Robert C. Martin",
+      blogs: 3
+    }
+
+    assert.deepStrictEqual(result, mostBlogs_Assert)
+  })
+
+  test('Con la funcion hecha con lodash', () => {
+    const result = listHelper.mostBlogs2(blogs)
+    logger.info(result)
+    const mostBlogs_Assert = {
+      author: "Robert C. Martin",
+      blogs: 3
+    }
+
+    assert.deepStrictEqual(result, mostBlogs_Assert)
+  })
+
+})
+
+describe('Most Likes', () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+  test('con la lista de blogs completa', () => {
+    const result = listHelper.mostLikes(blogs)
+    logger.info(result)
+    const mostLikes_Assert = {
+      author: "Edsger W. Dijkstra",
+      likes: 17
+    }
+
+    assert.deepStrictEqual(result, mostLikes_Assert)
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -28,11 +30,70 @@ const favoriteBlog = (blogs) => {
         "author": blogFavorito.author,
         "likes": blogFavorito.likes
     }
+}
 
+const mostBlogs = (blogs) => {
+    let dicConteo = {}
+
+    for (const blog of blogs){
+        const autor = blog.author
+        if (autor in dicConteo){
+            dicConteo[autor] += 1
+        }
+        else {
+            dicConteo[autor] = 1
+        }
+    }
+
+    const arrayPares = Object.entries(dicConteo)
+    let autorMasBlogs = ''
+    let numMasBlogs = 0
+    for (const par of arrayPares){
+        const [autor, numBlogs] = par
+        if (numBlogs > numMasBlogs){
+            autorMasBlogs = autor
+            numMasBlogs = numBlogs
+        }
+     }
+
+     return {
+        author: autorMasBlogs,
+        blogs: numMasBlogs
+     }
+}
+
+const mostBlogs2 = (blogs) => {
+    const dicConteo = _.countBy(blogs, 'author')
+    const arrayPares = Object.entries(dicConteo)
+    const [autorMasBlogs, masBlogs] = _.maxBy(arrayPares, ( [autor, blogs] ) => blogs)
+    return {
+        author: autorMasBlogs,
+        blogs: masBlogs
+    }
+}
+
+const mostLikes = (blogs) => {
+    const agrupacionAutor = _.groupBy(blogs, 'author')
+    const arrayParesAgrupacion = Object.entries(agrupacionAutor)
+
+    const arrayMostLikes = []
+    for (const [autor, arrayBlogs] of arrayParesAgrupacion) {
+        const sumLikes = _.sumBy(arrayBlogs, 'likes')
+        arrayMostLikes.push({
+            author: autor,
+            likes: sumLikes
+        })
+    }
+
+    const autorMasLikes = _.maxBy(arrayMostLikes, ( {author, likes} ) => likes)
+    return autorMasLikes
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostBlogs2,
+    mostLikes
 }
